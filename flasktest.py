aca80807615e5a075d39ef59263e6b21cb2e01d2
ali <alishibli97@hotmail.com>
2021-10-20 13:19:39 +0200

    model class and random sampling

--- flasktest.py
+++ flasktest.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, request
-import suggestion_model
+from suggestion_model import SuggestionModel
 app = Flask(__name__)
 
-songlist = suggestion_model.get_10_songs() # get the 10 new songs based on nothing, a bit unneccessery but
+model = SuggestionModel()
+songlist = model.get_10_songs(random_samples=True) # get the 10 new songs based on nothing, a bit unneccessery but
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -13,15 +14,13 @@ def index():
         global songlist
         for song in songlist:
 
-            #print(song, request.form.getlist(song))
             if request.form.get(song) != None:
                 checkedsongs.append(song)
 
-        #print(checkedsongs)
-        songlist = suggestion_model.get_10_songs(checkedsongs) # get the 10 new songs based on user input
+        songlist = model.get_10_songs(checkedsongs) # get the 10 new songs based on user input
         return render_template('form.html', songlist=songlist)
     else:
-        songlist = suggestion_model.get_10_songs() # get the 10 songs based on nothing
+        songlist = model.get_10_songs() # get the 10 songs based on nothing
         return render_template('form.html', songlist=songlist)
 
 app.run(host='0.0.0.0', port=5000, debug=True)
