a4ff0c8f90671420966543a84210db26bd6999a6
ali <alishibli97@hotmail.com>
2021-10-20 13:50:56 +0200

    flasktest.py

--- flasktest.py
+++ flasktest.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, request
-import suggestion_model
+from suggestion_model import SuggestionModel
 app = Flask(__name__)
 
-songlist = suggestion_model.get_10_songs() # get the 10 new songs based on nothing, a bit unneccessery but
+model = SuggestionModel()
+songlist = model.get_10_songs() # get the 10 new songs based on nothing, a bit unneccessery but
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -16,10 +17,10 @@ def index():
             if request.form.get(song) != None:
                 checkedsongs.append(song)
 
-        songlist = suggestion_model.get_10_songs(checkedsongs) # get the 10 new songs based on user input
+        songlist = model.get_10_songs(checkedsongs) # get the 10 new songs based on user input
         return render_template('form.html', songlist=songlist)
     else:
-        songlist = suggestion_model.get_10_songs() # get the 10 songs based on nothing
-        return render_template('form.html', songlist=songlist, user_vector=suggestion_model.user_vector)
+        songlist = model.get_10_songs() # get the 10 songs based on nothing
+        return render_template('form.html', songlist=songlist, user_vector=model.user_vector)
 
 app.run(host='0.0.0.0', port=5000, debug=True)
