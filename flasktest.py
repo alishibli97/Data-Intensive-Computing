9d2fc96f3728f2959eec6f01fabfea4d4a71baa0
Carina Wickström <49478094+carinawic@users.noreply.github.com>
2021-10-20 11:34:36 +0200

    fix bug with checkbox

--- flasktest.py
+++ flasktest.py
@@ -2,22 +2,26 @@ from flask import Flask, render_template, request
 import suggestion_model
 app = Flask(__name__)
 
+songlist = suggestion_model.get_10_songs()
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
 
     checkedsongs = []
-    songlist = suggestion_model.get_10_songs()
-
+    
     if request.method == 'POST':
+        global songlist
         for song in songlist:
+
+            #print(song, request.form.getlist(song))
             if request.form.get(song) != None:
                 checkedsongs.append(song)
-                print("Entered")
 
-        print(checkedsongs)
+        #print(checkedsongs)
         songlist = suggestion_model.get_10_songs(checkedsongs)
         return render_template('form.html', songlist=songlist)
     else:
+        songlist = suggestion_model.get_10_songs()
         return render_template('form.html', songlist=songlist)
 
 app.run(host='0.0.0.0', port=5000, debug=True)
